edaf9304c2f862dda2125cedcf5ad23dc7b7e8d7
smasefi <samanthamilan@gmail.com>
2024-06-13 11:15:17 -0400

    new branch

--- popculturetrivia/trivia/models.py
+++ popculturetrivia/trivia/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+# when using google auth - the login creates an instance of a user without having to explicitly create it
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -20,4 +22,10 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+    
+class Score(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
 
+    def __str__(self):
+        return f"Score for {self.user.username}: {self.score}"
